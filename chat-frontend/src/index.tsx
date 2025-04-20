fa9013dc6d6c5be98b9d528c78b1a93a0452c4ac
SanketP2003 <1002sanketpatil@gmail.com>
2025-04-20 20:34:51 +0530

    initial commit

--- chat-frontend/src/index.tsx
+++ chat-frontend/src/index.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css"; // Ensure Bootstrap is imported
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
 root.render(
   <React.StrictMode>
     <App />
